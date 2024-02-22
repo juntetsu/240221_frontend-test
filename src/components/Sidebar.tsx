5917b1fc566d6f920290058658a56b2a3e5ad141
jun <juntetsu.1012@gmail.com>
2024-02-22 13:01:31 +0900

    タイトルクリックでハイライト

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Sidebar.css";
 import { noteType } from "../App";
+import {useState} from "react";
 
 const Sidebar = ({ notes }: { notes: noteType[] }) => {
+  const [selectedNote, setSelectedNote] = useState<number | null>(null);
+
+  const handleClick = (id: number) => {
+    setSelectedNote(id)
+  }
+  
   return (
     <aside className="sidebar">
       <div className="sidebar__inner">
@@ -12,38 +19,13 @@ const Sidebar = ({ notes }: { notes: noteType[] }) => {
         </h1>
         <ul className="sidebar__list">
           {notes.map((note) => (
-            <li className="sidebar__item" key={note.id}>
+            <li className={`sidebar__item ${selectedNote === note.id ? "active" : ""}`} key={note.id} onClick={() => handleClick(note.id)}>
               <h2 className="sidebar__title">{note.title}</h2>
               <button className="sidebar__item-delete-btn">
                 <img src="../public/delete.svg" alt="" />
               </button>
             </li>
           ))}
-
-          <li className="sidebar__item">
-            <h2 className="sidebar__title">こころ</h2>
-            <button className="sidebar__item-delete-btn">
-              <img src="../public/delete.svg" alt="" />
-            </button>
-          </li>
-          <li className="sidebar__item active">
-            <h2 className="sidebar__title">我輩は猫である</h2>
-            <button className="sidebar__item-delete-btn">
-              <img src="../public/delete.svg" alt="" />
-            </button>
-          </li>
-          <li className="sidebar__item">
-            <h2 className="sidebar__title">坊ちゃん</h2>
-            <button className="sidebar__item-delete-btn">
-              <img src="../public/delete.svg" alt="" />
-            </button>
-          </li>
-          <li className="sidebar__item">
-            <h2 className="sidebar__title">学問のすゝめ</h2>
-            <button className="sidebar__item-delete-btn">
-              <img src="../public/delete.svg" alt="" />
-            </button>
-          </li>
         </ul>
       </div>
       <div className="sidebar__btn-list">
